9bdb6f4655f62081c178ae6e9cbf5683e0d525e1
Franklin <fabonero@ssct.edu.ph>
2023-03-21 13:01:38 +0800

    changes the h1

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get('/', function(request, response) {
-   response.send("<h1>Hello Dockers</h1>");
+   response.send("<h1>Hello Docker Compose</h1>");
 })
 
 app.listen(3000, function() {});
